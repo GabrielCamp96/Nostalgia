c66758e02a9d0d54a0a473a5860985b5224e9782
Gabriel Campanário <gabriel.camp96@hotmail.com>
2020-04-23 15:11:21 -0300

    efeitos de hover configurados

---
+++ src/js/navbar.js
@@ -0,0 +1,9 @@
+$('.dropdown-item').mouseenter(function() {
+    $('.dropdown-toggle').css({'color':'#F0FF00'})
+}
+);
+
+$('.dropdown-item').mouseout(function() {
+    $('.dropdown-toggle').css({'color':'white'})
+}
+);
